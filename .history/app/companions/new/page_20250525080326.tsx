ba4bb40533aff4450f3137cf09647d7db6809552
Code With Luka Suave (CodeWithLuka) <lukasuave.nextai@gmail.com>
2025-05-25 08:10:04 +0200

    Implement Feature Limit For SubScriptions

--- .history/app/companions/new/page_20250525080326.tsx
+++ .history/app/companions/new/page_20250525080326.tsx
@@ -31,10 +31,10 @@ const NewCompanionPage = async () => {
 						height={230}
 					/>
 					<div className="cta-badge">Upgrade your plan</div>
-					<h1>You&apos;ve Reached Your Limit</h1>
+					<h1>You’ve Reached Your Limit</h1>
 					<p>
-						You&apos;ve reached your companion limit. Upgrade to
-						create more companions and premium features.
+						You’ve reached your companion limit. Upgrade to create
+						more companions and premium features.
 					</p>
 					<Link
 						href="/subscription"
