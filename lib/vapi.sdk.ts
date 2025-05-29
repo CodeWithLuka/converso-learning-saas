60674f8e39fa65b6e3928ab2da8180a8e924f8bb
Code With Luka Suave (CodeWithLuka) <lukasuave.nextai@gmail.com>
2025-05-29 16:00:58 +0200

    Implement Limit

--- lib/vapi.sdk.ts
+++ lib/vapi.sdk.ts
@@ -1,3 +1,9 @@
 import Vapi from "@vapi-ai/web";
 
 export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN!);
+// Check if token exists
+console.log("VAPI TOKEN:", process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN);
+
+if (!process.env.NEXT_PUBLIC_VAPI_WEB_TOKEN) {
+	throw new Error("Missing VAPI web token environment variable");
+}
